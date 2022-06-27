afde024106c5bde1d988a2e1491593872e7ce5e2
Sachin Acharya <076bct060.sachin@pcampus.edu.np>
2022-06-27 07:22:36 +0545

    Experiment

--- lib/inputWidget.dart
+++ lib/inputWidget.dart
@@ -3,44 +3,47 @@
 import 'package:flutter/material.dart';
 
 class InputWidget extends StatelessWidget {
+  final Function fpointer;
+
+  InputWidget(this.fpointer);
   Widget build(BuildContext context) {
     return inputField();
   }
-}
 
-Widget inputField() {
-  String transactionString = "";
-  String amountString = "";
-  return Card(
-    elevation: 10,
-    borderOnForeground: true,
-    shadowColor: Colors.blueAccent,
-    child: Container(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          TextField(
-              onChanged: (value) {
-                transactionString = value;
+  Widget inputField() {
+    // String transactionString = "";
+    // String amountString = "";
+
+    final titleController = TextEditingController();
+    final amountController = TextEditingController();
+    return Card(
+      elevation: 10,
+      borderOnForeground: true,
+      shadowColor: Colors.blueAccent,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            TextField(
+                controller: titleController,
+                decoration: InputDecoration(
+                  labelText: "Transaction",
+                )),
+            TextField(
+              controller: amountController,
+              decoration: InputDecoration(labelText: "Amount"),
+            ),
+            TextButton(
+              child: Text("Add"),
+              onPressed: () {
+                fpointer(
+                    titleController.text, double.parse(amountController.text));
               },
-              decoration: InputDecoration(
-                labelText: "Transaction",
-              )),
-          TextField(
-            onChanged: (value) {
-              amountString = value;
-            },
-            decoration: InputDecoration(labelText: "Amount"),
-          ),
-          TextButton(
-            child: Text("Add"),
-            onPressed: () {
-              print(transactionString);
-            },
-          )
-        ],
+            )
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
